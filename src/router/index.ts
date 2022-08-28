a63ed125382b1c7b7846a4453cf676f6a2f3a9a6
TranSy <transy97@gmail.com>
2022-08-28 17:48:36 +0700

    init

---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    name: 'home',
+  },
+  {
+    path: '/about',
+    name: 'about',
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
